3ea6703c7ad663c641885a27676d8f96e6c4dc04
Josemi <josemi@dinan.es>
2021-10-23 11:43:28 +0200

    Data base and migrations (Basic)

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,14 +13,38 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('roles', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->boolean('visible');
+        });
+
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('role_id');
+            $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+
+            $table->foreign('role_id')->references('id')->on('roles');
+        });
+
+        Schema::create('access', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description');
+        });
+
+        Schema::create('roles_access', function (Blueprint $table) {
+            $table->unsignedBigInteger('role_id');
+            $table->unsignedBigInteger('access_id');
+            
+            $table->primary(['role_id','access_id']);
+
+            $table->foreign('role_id')->references('id')->on('roles');
+            $table->foreign('access_id')->references('id')->on('access');
         });
     }
 
@@ -31,6 +55,9 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
+        Schema::dropIfExists('roles_access');
+        Schema::dropIfExists('access');
         Schema::dropIfExists('users');
+        Schema::dropIfExists('roles');
     }
 }
