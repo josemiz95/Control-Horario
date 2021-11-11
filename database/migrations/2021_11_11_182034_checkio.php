415fa568bdf93aa75d0346a47f59ea6cf63a0267
Josemi Ruiz <jmruiz195@gmail.com>
2021-11-11 20:04:16 +0100

    Base de datos

---
+++ database/migrations/2021_11_11_182034_checkio.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Checkio extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('time_slots', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->date('date');
+            $table->time('total_time');
+            
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+
+        Schema::create('checks_io', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('slot_id');
+            $table->string('type'); // In / Out
+            $table->dateTime('check_time');
+            $table->text('coment');
+            $table->boolean('changed')->default(0);
+            $table->unsignedBigInteger('changed_by')->nullable();
+
+            $table->foreign('slot_id')->references('id')->on('time_slots')->onDelete('cascade');
+            $table->foreign('changed_by')->references('id')->on('users')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
